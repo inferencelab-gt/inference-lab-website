fac7058ca209794052c5d490646d588feb5c1876
inferencelab-gt <inferencelab.gt@gmail.com>
2025-11-13 17:33:08 -0500

    Update news.ts

--- src/data/news.ts
+++ src/data/news.ts
@@ -11,7 +11,7 @@ export interface NewsItem {
 export const news: NewsItem[] = [
   {
     title: "Inference Lab is hiring",
-    date: "13-11-2025",
+    date: "11-13-2025",
     description: "We are looking for multiple PhD students for fall 2026. We are looking for students with a strong passion and interest in AI-driven research, and a desire to work on cutting-edge problems in artificial intelligence, machine learning, and their applications to engineering.",
     image: "/group1.jpg",
   link: "/blog-docs/news/news_2/",
@@ -26,7 +26,7 @@ export const news: NewsItem[] = [
   },
   {
     title: "Hello world!",
-    date: "2025-06-15",
+    date: "08-06-2025",
     description: "We are excited to welcome several new PhD students and postdocs to the lab.",
     image: "/group3.jpg",
     link: "#"
