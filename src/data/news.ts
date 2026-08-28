ee4272e9680566caaafec5df048336f5e5ccc3b7
Ferdous Alam <ferdous@gatech.edu>
2026-08-28 16:06:37 -0400

    Fix latest news links

--- src/data/news.ts
+++ src/data/news.ts
@@ -14,7 +14,7 @@ export const news: NewsItem[] = [
     date: "08-28-2026",
     description: "We are excited to welcome our new PhD, MS, and undergraduate students for the fall semester! Do-Gon Kim, Rafi Ahmed Sagor, and Jacob W Lee are the first cohort of PhD students at Inference Lab. Berk and Shubh Raval are MS students expecting to graduate in the fall, while Maitreyi Sarkar, Nivedita Rajendran, Connell J Bartling, Esteban, and Kevin D Ghobrial will work as undergraduate researchers on different lab projects.",
     image: "/group1.jpg",
-  link: "/blog-docs/news/news_2/",
+    link: "/news/0",
     focus: true // Highlight this news item
   },
   {
@@ -22,7 +22,7 @@ export const news: NewsItem[] = [
     date: "07-15-2026",
     description: "Inference Lab received the Woodruff Innovation Nexus award, a Georgia Tech mechanical engineering grant supporting high-risk, high-reward research. We are grateful for this recognition and excited to use the award to advance our research projects.",
     image: "/group2.jpg",
-    link: "#", 
+    link: "/news/1",
   },
   {
     title: "Two best paper awards at IDETC 2025",
